3041287e464451bd75d4db7f4057765b8132e12f
OwenMoss-y <144296272+OwenMoss-y@users.noreply.github.com>
2023-09-19 20:28:37 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np 
+import random
+import runpy
+import sys
+
+sys.models['turtle'] = sys.modules['mockturtle']
+
+
+def test_ant[]:
+    random.seed(0)
+    mockturtle.events[:] = [('timer',)] * 600
+    runpy.run_module('freegames.ant')
+
